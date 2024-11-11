a5acd308b90554d9d8ed04fdf7ff5932ef3de45e
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-11-11 20:26:51 +0200

    Drop support for EOL Twitter

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-python3 randimgbot.py --yaml ~/bin/data/flagfacts.yaml --no-web  -i "~/github/flagfactsflags/flags/*.png" -t "{0}" --chance 6 --mastodon -a "Flag of {0}"
-python3 randimgbot.py --yaml ~/bin/data/flagfacts.yaml --no-web  -i "~/github/flagfactsflags/flags/*.png" -t "{0}" --chance 6 --twitter
+python3 randimgbot.py --yaml ~/bin/data/flagfacts.yaml --no-web  -i "~/github/flagfactsflags/flags/*.png" -t "{0}" --chance 6 -a "Flag of {0}"
